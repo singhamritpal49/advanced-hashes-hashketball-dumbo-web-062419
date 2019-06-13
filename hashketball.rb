b6135565611313afd15105bdb42e032be4744068
Amritpal Singh <singhamritpal49@gmail.com>
2019-06-13 21:10:47 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,12 @@
-# Write your code here!
+def game_hash
+  hash_game = {
+    :home => {:team_name => "Brooklyn Nets"},
+              :colors => ["Black", "White"]
+              :players => [
+                {player_name: "Alan Anderson", number: 0, shoe: 16, points: 22, rebounds: 12,
+                  assists: 12, steals: 3, blocks:1, slam_dunks: 1},
 
+              ]
+    }
 
-
-
-
-
-
-
-
-
+  end
